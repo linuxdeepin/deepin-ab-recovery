4ec8296d996f04e17e893587491bc5fdb7e48541
luokai <luokai@uniontech.com>
2020-08-05 04:47:25 +0200

    fix(main): 拼接系统信息字符串
    解析lsb_release命令，拆分Description字符串后在os和版本号之间拼接osProductType字段
    
    Log: 解决系统备份后，在Grub 界面信息显示不正确bug
    Bug: https://pms.uniontech.com/zentao/bug-view-40466.html
    Reviewed-on: http://gerrit.uniontech.com/c/deepin-ab-recovery/+/1282
    Reviewed-by: wubowen <wubowen@uniontech.com>
    Reviewed-by: <mailman@uniontech.com>
    Tested-by: <mailman@uniontech.com>
    Change-Id: I107dbe2cd66c8277ec27ea3f4cda8d86d917ccbc
    Reviewed-on: http://gerrit.uniontech.com/c/deepin-ab-recovery/+/7584

--- main.go
+++ main.go
@@ -318,7 +318,7 @@ func backup(cfg *Config, envVars []string) error {
 			systemName := osReleaseInfo[osSystemName]
 			majorVersion := osReleaseInfo[osMajorVersion]
 			EditName := osReleaseInfo[osEditionName]
-			osDesc = systemName + " " + majorVersion + "" + EditName
+			osDesc = systemName + " " + majorVersion + " " + EditName
 		}
 	}
 
